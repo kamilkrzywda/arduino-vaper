1d445c855e0faaa263320b9a41c92f87a686ef82
Kamil Krzywda <krzywda.kamil@gmail.com>
2016-03-10 22:19:58 +0100

    obsluga wersji bez miernika oporu grzalki

--- no-ohmmeter/no-ohmmeter.ino
+++ no-ohmmeter/no-ohmmeter.ino
@@ -1,7 +1,6 @@
 #include <EEPROM.h>
 
 //coil resistance and batery voltage sensors
-const int coilSensor = A0;
 const int batterySensor = A1;
 
 //buttons
@@ -17,24 +16,22 @@ const int led1 = 3;
 const int led2 = 5;
 const int led3 = 6;
 
+int led1Light = 0;
+int led2Light = 0;
+int led3Light = 0;
+
 //buttons
 int button1Status = 0;
 int button2Status = 0;
 int heaterButtonStatus = 0;
 
 //sensors
-int coilSensorValue = 0;
 int batterySensorValue = 0;
-
-
-//power calculations
-float coilOhms = 1.00;
 float batteryVoltage = 8.00;
 
-float powerMultiplier = 2;
+//power
 int powerSet = 0;
-float powerMax = 20;
-float powerPWM = 0;
+int powerPWM = 0;
 
 //heating
 int heating = 0;
@@ -43,50 +40,46 @@ int heating = 0;
 int sleep = 0;
 
 /*
- * increase power setting
+ * change  power setting
  */
-void addPower(int setPower){
-  powerSet+=setPower;
-  if(powerSet > 31){
-    powerSet = 31;
-  }
-  EEPROM.write(0, powerSet);
-  setLeds();
-}
-
-/*
- * decrease power setting
- */
-void subPower(int setPower){
-  powerSet-=setPower;
-  if(powerSet < 1){
+void setPower(int power){
+  powerSet = power;
+  if(powerSet > 193){
+    powerSet = 193;
+    powerPWM = 255;
+  }else if(powerSet < 1){
     powerSet = 1;
+  }else{
+    powerPWM = powerSet;
   }
   EEPROM.write(0, powerSet);
-  setLeds();
 }
 
-/*
- * update status leds
- */
+void updateLeds(){
+  analogWrite(led1,led1Light);
+  analogWrite(led2,led2Light);
+  analogWrite(led3,led3Light);
+}
+
 void setLeds(){
-  if(powerSet <= 10){
-    analogWrite(led1, powerSet*powerSet*2);
-    analogWrite(led2,0);
-    analogWrite(led3,0);
-  }else if(powerSet <= 20){
-    analogWrite(led1,255);
-    analogWrite(led2, (powerSet-10)*(powerSet-10)*2);
-    analogWrite(led3,0);
-  }else if(powerSet <= 30){
-    analogWrite(led1,255);
-    analogWrite(led2,255);
-    analogWrite(led3, (powerSet-20)*(powerSet-20)*2);
+  if(powerSet <= 64){
+    led1Light = powerSet*powerSet/32;
+    led2Light = 0;
+    led3Light = 0;
+  }else if(powerSet <= 128){
+    led1Light = 255;
+    led2Light = (powerSet-64)*(powerSet-64)/32;
+    led3Light = 0;
+  }else if(powerSet <= 192){
+    led1Light = 255;
+    led2Light = 255;
+    led3Light = (powerSet-128)*(powerSet-128)/32;
   }else{
-    analogWrite(led1,255);
-    analogWrite(led2,0);
-    analogWrite(led3,255);
+    led1Light = (sin(((float)millis()+1000)/500)+1)*100;
+    led2Light = (sin(((float)millis()+500)/500)+1)*100;
+    led3Light = (sin(((float)millis())/500)+1)*100;
   }
+  updateLeds();
 }
 
 /*
@@ -103,21 +96,8 @@ void readStatus(){
   }
   
   if(heating == 0){
-    coilSensorValue = analogRead(coilSensor);
     batterySensorValue = analogRead(batterySensor);
-    
-    batteryVoltage = ((float)batterySensorValue)/18;
-    coilOhms = (float)(coilSensorValue-120)/100;
-    powerMax = batteryVoltage*batteryVoltage/coilOhms;
-    powerPWM = powerSet/powerMax;
-    if(powerPWM > 1){
-      powerPWM = 1;
-    }
-    if(powerSet == 31){
-      powerPWM = 1;
-    }
-    
-    serialDebug();
+    batteryVoltage = (float)batterySensorValue/37;
   }
 }
 
@@ -125,16 +105,16 @@ void readStatus(){
  * buttons actions
  */
 void checkButtons(){
-  if(button1Status < 500){
+  if(button1Status < 900){
     sleep = 0; 
-    addPower(1);
-    delay(50);
+    setPower(powerSet+1);
+    delay(10);
   }
   
-  if(button2Status < 500){
+  if(button2Status < 900){
     sleep = 0;
-    subPower(1);
-    delay(50);
+    setPower(powerSet-1);
+    delay(10);
   }
   
   if(heaterButtonStatus){
@@ -150,7 +130,8 @@ void checkButtons(){
 void heat(){
   if(heating > 0){
     if(batteryVoltage > 7){
-      analogWrite(heaterOutput,255-255*powerPWM);
+      analogWrite(heaterOutput,255-powerPWM);
+      Serial.println(255-powerPWM);
       if(heating > 100){
         heating = 0;
         digitalWrite(heaterOutput,HIGH);
@@ -182,42 +163,42 @@ void serialDebug(){
   Serial.print("\t");
   Serial.print(heating);
   Serial.print("\t");
-  Serial.print("coil ohms: ");
-  Serial.print(coilOhms);
-  Serial.print("\t");
-  Serial.print(coilSensorValue);
-  Serial.print("\t");
   Serial.print("battery volatege: ");
   Serial.print(batteryVoltage);
   Serial.print("\t");
-  Serial.print("max power: ");
-  Serial.print(powerMax);
+  Serial.print("power set: ");
+  Serial.print(powerSet);
   Serial.print("\t");
   Serial.print("power pwm: ");
   Serial.print(powerPWM);
+  Serial.print("\t");
+  Serial.print("millis: ");
+  Serial.print(millis());
   Serial.println("");
 }
 
 void setup(){
   Serial.begin(115200);
-  addPower(EEPROM.read(0));
+  setPower(EEPROM.read(0));
   digitalWrite(heaterOutput,HIGH);
 }
 
 void loop() {
   sleep++;
   
- readStatus();
+  readStatus();
 
   if(sleep == 20000){
     analogWrite(led1,0);
     analogWrite(led2,0);
     analogWrite(led3,0);
   }else if(sleep < 20000){
+    setLeds();
     checkButtons();
     heat();
   }
   
-  delay(1);
+  delay(2);
   
+  //serialDebug();
 }
